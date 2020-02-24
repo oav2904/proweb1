879625e8be53ed463bb2cefed3367cb0c0ea9ddf
oav2904 <oscariasv@gmail.com>
2020-02-23 23:58:22 -0600

    cambios varios

---
+++ bs.sql
@@ -0,0 +1,15 @@
+
+create table users (
+
+	id serial primary key,
+	f_name text not null,
+	s_name text default ' ',
+	f_lastname text not null,
+	s_lastname text default ' ',
+	password text not null,
+	birthday date not null,
+	img bytea not null,
+	about text not null,
+	position text not null,
+	location text not null 
+);
--- register.php
+++ register.php
@@ -20,73 +20,130 @@ require_once './shared/db.php';
         exit();
     }
  ?>
+        <form method="POST" enctype="multipart/form-data">
 
-        <form method="POST">
             <div class="field">
               <label class="label">Name</label>
-              <div class="control">
-                <input class="input" type="text" placeholder="Text input">
-              </div>
-            </div>
-
-            <div class="field">
-              <label class="label">Username</label>
               <div class="control has-icons-left has-icons-right">
-                <input class="input is-success" type="text" placeholder="Text input" value="">
+                <input class="input" type="text" placeholder="Text input" value="">
                 <span class="icon is-small is-left">
                   <i class="fas fa-user"></i>
                 </span>
                 <span class="icon is-small is-right">
                   <i class="fas fa-check"></i>
                 </span>
-              </div>
-              <p class="help is-success">This username is available</p>
-            </div>
+             
+            <div class="field">
+              <label class="label">Middlename</label>
+              <div class="control has-icons-left has-icons-right">
+                <input class="input" type="text" placeholder="Text input" value="">
+                <span class="icon is-small is-left">
+                  <i class="fas fa-user"></i>
+                </span>
+                <span class="icon is-small is-right">
+                  <i class="fas fa-check"></i>
+                </span>
+              
+            <div class="field">
+              <label class="label">First lastname</label>
+              <div class="control has-icons-left has-icons-right">
+                <input class="input" type="text" placeholder="Text input" value="">
+                <span class="icon is-small is-left">
+                  <i class="fas fa-user"></i>
+                </span>
+                <span class="icon is-small is-right">
+                  <i class="fas fa-check"></i>
+                </span>
+
+            <div class="field">
+              <label class="label">Second lastname</label>
+              <div class="control has-icons-left has-icons-right">
+                <input class="input" type="text" placeholder="Text input" value="">
+                <span class="icon is-small is-left">
+                  <i class="fas fa-user"></i>
+                </span>
+                <span class="icon is-small is-right">
+                  <i class="fas fa-check"></i>
+                </span>   
+
+            <div class="field">
+              <label class="label">Position</label>
+              <div class="control has-icons-left has-icons-right">
+                <input class="input" type="text" placeholder="Text input" value="">
+                <span class="icon is-small is-left">
+                  <i class="fas fa-user"></i>
+                </span>
+                <span class="icon is-small is-right">
+                  <i class="fas fa-check"></i>
+                </span>  
+                <div class="field">
+              <label class="label">Birthday</label>
+              <div class="control has-icons-left has-icons-right">
+                <input class="input" type="date"  value="">
+                <span class="icon is-small is-left">
+                  <i class="fas fa-user"></i>
+                </span>
+                <span class="icon is-small is-right">
+                  <i class="fas fa-check"></i>
+                </span>    
 
             <div class="field">
               <label class="label">Email</label>
               <div class="control has-icons-left has-icons-right">
-                <input class="input is-danger" type="email" placeholder="Email input" value="hello@">
+                <input class="input" type="email" placeholder="Email input" value="">
                 <span class="icon is-small is-left">
                   <i class="fas fa-envelope"></i>
                 </span>
                 <span class="icon is-small is-right">
                   <i class="fas fa-exclamation-triangle"></i>
                 </span>
-              </div>
-              <p class="help is-danger">This email is invalid</p>
+           <div class="field">
+                <label class="label">Password</label>
+                <div class="control has-icons-left has-icons-right">
+                    <input class="input" name="password" type="password" placeholder="Email input" value="">
+                    <span class="icon is-small is-left">
+                        <i class="fas fa-lock"></i>
+                    </span>
+                    <span class="icon is-small is-right">
+                        <i class="fas fa-exclamation-triangle"></i>
+                    </span>
+                </div>
+            </div>
+            <div class="field">
+                <label class="label">Re~Password</label>
+                <div class="control has-icons-left has-icons-right">
+                    <input class="input" name="repassword" type="password" placeholder="Email input" value="">
+                    <span class="icon is-small is-left">
+                        <i class="fas fa-lock"></i>
+                    </span>
+                    <span class="icon is-small is-right">
+                        <i class="fas fa-exclamation-triangle"></i>
+                    </span>
+                </div>
             </div>
 
-
+            <div id="file-js-example" class="file has-name">
+                <label class="file-label">
+                    <input class="file-input" type="file" name="resume" accept="image/png, .jpeg, .jpg " autofocus >
+                <span class="file-cta">
+                <span class="file-icon">
+                    <i class="fas fa-upload"></i>
+                </span>
+                <span class="file-label">
+                    Choose a image
+                </span>
+                </span>
+                <span class="file-name">
+                  No file uploaded
+                </span>
+              </label>
+            </div>
             <div class="field">
               <label class="label">About me</label>
               <div class="control">
-                <textarea class="textarea" placeholder="Textarea" required></textarea>
+                <textarea class="textarea has-fixed-size" required></textarea>
               </div>
             </div>
-
-            <div class="field">
-              <div class="control">
-                <label class="checkbox">
-                  <input type="checkbox">
-                  I agree to the <a href="#">terms and conditions</a>
-                </label>
-              </div>
-            </div>
-
-            <div class="field">
-              <div class="control">
-                <label class="radio">
-                  <input type="radio" name="question">
-                  Yes
-                </label>
-                <label class="radio">
-                  <input type="radio" name="question">
-                  No
-                </label>
-              </div>
-            </div>
-
             <div class="field is-grouped">
               <div class="control">
                 <button class="button is-link">Submit</button>
@@ -95,6 +152,17 @@ require_once './shared/db.php';
                 <button class="button is-link is-light">Cancel</button>
               </div>
             </div>
+           
+
+            <script>
+              const fileInput = document.querySelector('#file-js-example input[type=file]');
+              fileInput.onchange = () => {
+                if (fileInput.files.length > 0) {
+                  const fileName = document.querySelector('#file-js-example .file-name');
+                  fileName.textContent = fileInput.files[0].name;
+                }
+              }
+            </script>
         </form>
     </div>
 </section>
